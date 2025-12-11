c7cb7efe4e3ed66f142979286b74e0be8208e504
Ankith289 <01fe23bec148@kletech.ac.in>
2025-12-11 14:09:23 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,35 @@
-echo hi
-chmod +x xsens_MTI710/permissions.sh 
-./permissions
+#!/bin/bash
+set -e
+
+echo "hi"
+chmod +x xsens_MTI710/permissions.sh
+./xsens_MTI710/permissions.sh
 sleep 5
+
+# -------------------------------
+# ENV 1: For complete_track_geometry
+# -------------------------------
 python -m venv env
-pip install -r xsens_MTI710/requirements.txt
+sudo chown -R $USER:$USER /home/nvidia/sih2025_black/env
 source env/bin/activate
+pip install -r xsens_MTI710/requirements.txt
+
 g++ xsens_MTI710/raw_data_reciver.cpp -o xsens_MTI710/mti710_force_log -lusb-1.0 -O2 -pthread
+
 rm -f /tmp/xsens_pipe
 mkfifo /tmp/xsens_pipe
+
 rm -f /tmp/track_geo
 mkfifo /tmp/track_geo
-./xsens_MTI710/mti710_force_log > /tmp/xsens_pipe |python3 tof_stm32_interface/complete_track_geometry.py
+
 deactivate
+
+# -------------------------------
+# ENV 2: For ml.py
+# -------------------------------
 python -m venv envs
+sudo chown -R $USER:$USER /home/nvidia/sih2025_black/envs
 source envs/bin/activate
 pip install -r condition/requirements.txt
-/home/nvidia/env_s/bin/python3 condition/ml.py
+deactivate
+
