c6cda3954e40036b3689f9932dd6031851e1e7c8
Ankith289 <01fe23bec148@kletech.ac.in>
2025-12-11 14:08:51 +0530

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,35 +1,3 @@
-#!/bin/bash
-set -e
-
-echo "hi"
-chmod +x xsens_MTI710/permissions.sh
-./xsens_MTI710/permissions.sh
-sleep 5
-
-# -------------------------------
-# ENV 1: For complete_track_geometry
-# -------------------------------
-python -m venv env
-source env/bin/activate
-pip install -r xsens_MTI710/requirements.txt
-
-g++ xsens_MTI710/raw_data_reciver.cpp -o xsens_MTI710/mti710_force_log -lusb-1.0 -O2 -pthread
-
-rm -f /tmp/xsens_pipe
-mkfifo /tmp/xsens_pipe
-
-rm -f /tmp/track_geo
-mkfifo /tmp/track_geo
-
-deactivate
-
-# -------------------------------
-# ENV 2: For ml.py
-# -------------------------------
-python -m venv envs
-source envs/bin/activate
-pip install -r condition/requirements.txt
-deactivate
 
 # ================================================
 # Open TERMINAL 1 → run complete_track_geometry loop
