c8d238fc8eac14e34cb17db20a7655846e22b138
Ankith289 <01fe23bec148@kletech.ac.in>
2025-12-11 23:57:02 +0530

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,6 +1,14 @@
-
 # ================================================
 # Open TERMINAL 1 → run complete_track_geometry loop
+# ================================================
+gnome-terminal -- bash -c "
+python3 master/uart_time_mux.py;
+exec bash
+"
+
+
+# ================================================
+# Open TERMINAL 2 → run complete_track_geometry loop
 # ================================================
 gnome-terminal -- bash -c "
 source env/bin/activate;
@@ -9,7 +17,7 @@ exec bash
 "
 
 # ================================================
-# Open TERMINAL 2 → run ml.py loop
+# Open TERMINAL 3 → run ml.py loop
 # ================================================
 gnome-terminal -- bash -c "
 source envs/bin/activate;
@@ -17,7 +25,7 @@ python3 condition/ml.py;
 exec bash
 "
 # ================================================
-# Open TERMINAL 3 → run complete_track_geometry loop
+# Open TERMINAL 4 → run Acceleration
 # ================================================
 gnome-terminal -- bash -c "
 source env/bin/activate;
@@ -25,3 +33,29 @@ source env/bin/activate;
 exec bash
 "
 
+# ================================================
+# Open TERMINAL 5 → run lidar
+# ================================================
+gnome-terminal -- bash -c "
+source envl/bin/activate;
+python3 lidar/complete_solution.py;
+exec bash
+"
+
+# ================================================
+# Open TERMINAL 6 → run rearwindow
+# ================================================
+gnome-terminal -- bash -c "
+source envs/bin/activate;
+python3 rear_window/ardu.py;
+exec bash
+"
+
+# ================================================
+# Open TERMINAL 7 → run reciever.py
+# ================================================
+gnome-terminal -- bash -c "
+source envr/bin/activate;
+python3 reciever.py;
+exec bash
+"
