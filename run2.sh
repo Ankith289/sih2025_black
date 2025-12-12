f93a5c2f66ddb3a896adb444f1bf64f8a81cf2c9
Ankith289 <01fe23bec148@kletech.ac.in>
2025-12-12 07:44:38 +0530

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -6,7 +6,6 @@ python3 master/uart_time_mux.py;
 exec bash
 "
 
-
 # ================================================
 # Open TERMINAL 2 → run complete_track_geometry loop
 # ================================================
@@ -24,26 +23,9 @@ source envs/bin/activate;
 python3 condition/ml.py;
 exec bash
 "
-# ================================================
-# Open TERMINAL 4 → run Acceleration
-# ================================================
-gnome-terminal -- bash -c "
-source env/bin/activate;
-./xsens_MTI710/mti710_force_log > /tmp/xsens_pipe | python3 Acceleration/track_accel.py;
-exec bash
-"
 
 # ================================================
-# Open TERMINAL 5 → run lidar
-# ================================================
-gnome-terminal -- bash -c "
-source envl/bin/activate;
-python3 lidar/complete_solution.py;
-exec bash
-"
-
-# ================================================
-# Open TERMINAL 6 → run rearwindow
+# Open TERMINAL 4 → run rail profile
 # ================================================
 gnome-terminal -- bash -c "
 source envs/bin/activate;
@@ -52,7 +34,34 @@ exec bash
 "
 
 # ================================================
-# Open TERMINAL 7 → run reciever.py
+# Open TERMINAL 5 → run Acceleration
+# ================================================
+gnome-terminal -- bash -c "
+source envl/bin/activate;
+python3 rail_profile/stero.py;
+exec bash
+"
+
+# ================================================
+# Open TERMINAL 6 → run lidar
+# ================================================
+gnome-terminal -- bash -c "
+source envl/bin/activate;
+python3 lidar/complete_solution.py;
+exec bash
+"
+
+# ================================================
+# Open TERMINAL 7 → run rearwindow
+# ================================================
+gnome-terminal -- bash -c "
+source envs/bin/activate;
+python3 rear_window/ardu.py;
+exec bash
+"
+
+# ================================================
+# Open TERMINAL 8 → run reciever.py
 # ================================================
 gnome-terminal -- bash -c "
 source envr/bin/activate;
